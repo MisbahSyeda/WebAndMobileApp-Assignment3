85159b4aa6d13efa52bf846254d62a9cb64c5514
MisbahSyeda <misbahsyeda78@gmail.com>
2020-05-08 13:27:27 +0500

    Web And Mobile App Dev Assignment 3

---
+++ index.js
@@ -0,0 +1,29 @@
+// number count for stats, using jQuery animate
+
+$('.counting').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+    
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+  
+    {
+  
+      duration: 4000,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+       
+      }
+  
+    });  
+    
+  
+  });
+  //for gallery
+  baguetteBox.run('.tz-gallery');
+
